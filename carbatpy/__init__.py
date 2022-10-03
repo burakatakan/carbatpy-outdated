ac248ebf683148012c1db98f61854ff0616a608d
Burak Atakan <b.atakan@uni-due.de>
2022-10-03 18:37:58 +0200

    start to switch towards the low-level interface of CoolProp

---
+++ carbatpy/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  3 11:35:21 2022
+
+@author: atakan
+"""
+
+import os
+import sys
+
+directories =("src/run_scripts", "src/analysis", "data", "tests")
+for d_name in directories:
+    fpath = os.path.join(os.path.dirname(__file__), d_name)
+    sys.path.append(fpath)
+print(sys.path)
