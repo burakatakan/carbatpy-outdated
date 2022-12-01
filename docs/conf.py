119922c01fc041381712a14900947f047defaa94
burakatakan <35733406+burakatakan@users.noreply.github.com>
2022-12-01 18:47:01 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -49,7 +49,7 @@ templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # numpydoc_show_classmembers = False
-autodoc_mock_imports = ['ctREFPROP', 'CoolProp', 'matplotlib', 'scipy', 'pandas']
+autodoc_mock_imports = ['numpy', 'ctREFPROP', 'CoolProp', 'matplotlib', 'scipy', 'pandas']
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
