6a30db9463805e8805fdd83d8b9fd30ab8361c15
Burak Atakan <b.atakan@uni-due.de>
2022-12-06 15:47:50 +0100

    included function for creating a filename and a legend for mixtures

--- carbatpy/mixture_search.py
+++ carbatpy/mixture_search.py
@@ -21,7 +21,7 @@ Tcond = 273.15+60
 Tin = 273.15 + 20
 Tout = 273.15 + 131.5
 
-fluid_s = "Propane * Pentane * Butane"
+fluid_s = "Propane * Hexane * Butane"
 x0=.55
 x1 = 1-x0 -.02
 comp = [x0, x1, 1-x0-x1]# [.4, 0.5 , 0.1]
@@ -60,8 +60,12 @@ def hp_calc(pev, pcond, comp =[1.0], dT_cin=10, fluid_s =fluid_s, eta_s =0.65):
     cond = fprop.p_prop_sat(pcond, fluid_s, composition = comp, option=1) # condenser pressure
     npo =10
     con =[]
-    for h in np.linspace(cond[0,2],cond[1,2],npo):
-        con.append(fprop.hp(h, cond[0,1],fluid_s, composition = comp, option=1))
+    eva = []
+    for h in np.linspace(cond[0, 2],cond[1, 2], npo):
+        con.append(fprop.hp(h, cond[0, 1],fluid_s, composition = comp, option=1))
+    for h in np.linspace(evap[1, 2], evap[0, 2], npo):
+        eva.append(fprop.hp(h, evap[0, 1],fluid_s, composition = comp, option=1))
+    
     # isentropic state after compressor
     comp_s = fprop.sp(comp_in[ 4], cond[0, 1], fluid_s, composition=comp) 
     dhs = comp_s[2] - comp_in[2]
@@ -71,7 +75,7 @@ def hp_calc(pev, pcond, comp =[1.0], dT_cin=10, fluid_s =fluid_s, eta_s =0.65):
     
     
     return np.array([evap[0,:], comp_in, comp_out, *con,
-                     evap[1],evap[0,:]])
+                     *eva])
 
 def find_comp(x, x0, p=1.1e5, T=289.):
     """
@@ -103,20 +107,47 @@ def find_comp(x, x0, p=1.1e5, T=289.):
         return evap[0,0] - T
     else:
         return 1e6
+
+def f_name(fluid_s, comp, p0, p_ratio):
+        n0 = fluid_s.split(" * ")
+        fn =""
+        leg= ""
+        for i, n in enumerate(n0):
+            fn+="%2i"%(comp[i]*100) + n
+            leg+=n[0]+str(int(comp[i]*100))+" "
+        fn+="%2i_%i"%(p0/1e4, p_ratio)
+        fn=fn.split(" ")
+        fn =fn[0]+fn[1]
+        fn+=".png"
+        return "data/"+fn, leg
+    
     
 
 if __name__ == "__main__":
     # calculate a heat pump cycle for a given composition and pressure levels
-    res = hp_calc(p0, p0 * p_ratio, comp= comp)
+    kJ =1/1000
+    
+    res = hp_calc(p0, p0 * p_ratio, comp = comp)
+    pos_shift = np.where(res[:,0]==min(res[:,0]))[0]
     f, ax = plt.subplots(1)
-    ax.plot(res[:,2], res[:,0])
+    
     # find the ternary composition for given compressor inlet T & p:
     loes = opti.root(find_comp, x1, args=(x0,1.5e5, 290.))
+    fn, leg = f_name(fluid_s, comp, p0, p_ratio)
+    ax.plot((res[:,2]-res[pos_shift,2]) * kJ, res[:,0], "b-o", label=leg)
+    comp0 = comp
     comp_n = x0, loes.x[0], 1-x0-loes.x[0]
     print (fluid_s, "%1.3f:%1.3f:%1.3f"%(comp_n[:]))
     print("success: %7s, result: %1.3f"%( loes.success, loes.x))
     res = hp_calc(p0, p0 * p_ratio, comp= [x0,loes.x, 1-x0-loes.x])
-    f2, ax2 = plt.subplots(1)
-    ax2.plot(res[:,2]/1000, res[:,0])
+    # f2, ax2 = plt.subplots(1)
+    fn, leg = f_name(fluid_s, comp_n, p0, p_ratio)
+    ax.plot((res[:, 2]-res[pos_shift, 2]) * kJ, res[:, 0],"k-v", label=leg)
+    ax.set_xlabel("Enthalpy / (kJ/mol)")
+    ax.set_xlabel("Temperature / K")
+    ax.legend()
+    
+    f.savefig(fn)
+            
     
     
