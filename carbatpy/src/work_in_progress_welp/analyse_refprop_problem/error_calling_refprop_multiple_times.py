1a68a22158cd805490d5fd260635d9bc03903f69
purplechickpea <alexandra.welp@uni-due.de>
2023-04-14 10:58:11 +0200

    Commit: corrected error_example_reese.py for documentation (with Isobutane)

--- carbatpy/src/work_in_progress_welp/analyse_refprop_problem/error_calling_refprop_multiple_times.py
+++ carbatpy/src/work_in_progress_welp/analyse_refprop_problem/error_calling_refprop_multiple_times.py
@@ -18,20 +18,22 @@ def get_state(p, q, fluid, z=[1.0]):
 
 def try_fun(x, h, p, q, fluid, z=[1.0]):
     global counter_refprop
-    res = RP.REFPROPdll(fluid, "PH", "D;CP;VIS;TCX;PRANDTL;T", MASS_BASE_SI, 0, 0, p, h[1], z).Output[0:6]
+    res = RP.REFPROPdll(fluid, "PH", "D;CP;VIS;TCX;PRANDTL;T", MASS_BASE_SI, 0, 0, p, h, z).Output[0:6]
+    res2 = RP.REFPROPdll(fluid, "QH", "D;CP;VIS;TCX;PRANDTL;T", MASS_BASE_SI, 0, 0, q, h, z).Output[0:6]
     rho = res[0]
     T = res[5]
-    counter_refprop +=1
+    counter_refprop +=2
     if rho == -9999990.:
         print(f"counter_refprop: {counter_refprop}")
         raise ValueError("Unplausible state")
-    return [-0.5 * rho * T, 400000]
+    h_out_0 = 0.01 * T
+    return [h_out_0]
 
 counter_refprop = 0
 count = 0
 for i in range(call):
     count +=1
-    sol = solve_ivp(lambda x,h: try_fun(x, h, 1e6, 0.5, "Isobutane", [1.0]), [0,100], [4,503360])
+    sol = solve_ivp(lambda x,h: try_fun(x, h, 1e6, 0.5, "Isobutane", [1.0]), [0,100], [503360])
     if (count%(call/100)) == 0:
         print(f"{count*100/call} %")
 
