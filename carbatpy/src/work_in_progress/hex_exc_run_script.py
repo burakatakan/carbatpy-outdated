00bb6debcc73092a4bb51ffc752a279d56b340ac
Burak Atakan <atakan.thermodynamik.duisburg@gmail.com>
2023-05-19 11:58:38 +0200

    new log-file for heat-exchanger calculation introduced in csv-format
    I am not sure whether this is the best format, but, since the number of fluid components
    may be differeing, this seemed to be the best solution
    
    Signed-off-by: Burak Atakan <atakan.thermodynamik.duisburg@gmail.com>

--- carbatpy/src/work_in_progress/hex_exc_run_script.py
+++ carbatpy/src/work_in_progress/hex_exc_run_script.py
@@ -18,10 +18,11 @@ import pandas as pd
 import os
 import shutil
 from datetime import datetime
+import csv
 
 directory_path = os.path.dirname(os.path.abspath(__file__))
 resultsDir = "results"
-
+logFile = "HeatExchangerCalculations_log.csv"
 
 # Name of the excel-file
 y_file_name0 = "heat_exchanger_input1"
@@ -29,15 +30,17 @@ y_file_name = os.path.join(directory_path, y_file_name0)
 T0 = 298.15
 
 # read the yaml file as input
-neu = h_ex.st_heat_exchanger_input.read_hex_file(y_file_name+".xlsx", "HEXsimple")
+neu = h_ex.st_heat_exchanger_input.read_hex_file(
+    y_file_name+".xlsx", "HEXsimple")
 # The file should be copied to a file with the same name and the date added as below
 now = datetime.now()
 dt_string = now.strftime("_%Y_%m_%d_%H_%M_%S")
 
 resDir = os.path.join(directory_path, resultsDir)
 exists = os.path.exists(resDir)
-if not exists: os.makedirs(resDir)
-res_file_name =y_file_name+dt_string
+if not exists:
+    os.makedirs(resDir)
+res_file_name = y_file_name+dt_string
 
 shutil.copy(y_file_name+".xlsx", res_file_name+".xlsx")
 
@@ -50,7 +53,7 @@ print(f"Solution found: {res.success},  {res.message}")
 
 # plotting and evalution:
 # evaluate results (and plot)
-#y_file_name+="calc"
+# y_file_name+="calc"
 f1, f2, ds, dq = hex2.he_state(res, 6, res_file_name)
 
 
@@ -68,7 +71,6 @@ print("Exergy flow rate, entering: %3.3f W" % (ex_in))
 # copying the results to a results directory, including date and time
 
 
-
 dircont = os.listdir(directory_path)
 for file in dircont:
     allparts = file.split(".")
@@ -77,7 +79,17 @@ for file in dircont:
         if nbase == y_file_name0+dt_string:
             fname = nbase+dt_string+"."+nend
             resDir0 = os.path.join(resDir, fname)
-            if nend == "yaml" or nend =="xlsx":
-                shutil.copy(file, resDir0)
-            else:
-                shutil.move(file, resDir0)
+            shutil.move(file, resDir0)
+
+
+# write in logfile:
+try:
+    inp_dict = neu.__dict__.copy()
+    inp_dict["filename"] = res_file_name
+
+    with open(os.path.join(resDir, logFile), 'a') as csvfile:
+        writer = csv.DictWriter(csvfile, inp_dict)
+
+        writer.writerow(inp_dict)
+except IOError:
+    print("I/O error")
