5439f89421e259b1f6c95e48f743e2cee83823b4
Burak Atakan <atakan.thermodynamik.duisburg@gmail.com>
2023-05-20 08:46:55 +0200

    static heat-exchanger (no heat transfer calculation, only heating), pressure variation to reach the output condition
    The script is going towards a pure heat exchanger calculation (will soon be shifted towards components).
    So far the (evaporator) exit temperature of the  working fluid is given and pressure varied such,
    that the fluid entering as saturated liquid reaches this point. The mass flow rate ratio is calculated and checked whether the
    minimum approach temperature is >0. Other entering states (with quality >0) must be implemented.
    The script is not well commented yet.

--- carbatpy/src/run_scripts/heat_pump_linear.py
+++ carbatpy/src/run_scripts/heat_pump_linear.py
@@ -18,83 +18,113 @@ props = "REFPROP"
 # import components.heat_exchanger as hex
 
 
-class pinch_point:
-    def __init__(self, fluids, temps, ps,  points=30, dT_hex=0.5,
-                 dT_superh=10, props="REFPROP",
+class static_heat_exchanger:
+    def __init__(self, fluids, temps, ps, qs=[0, -2], points=30, dT_hex=0.5,
+                 dT_superh=10, heating =True, props="REFPROP",
                  compositions=[[1.0], [1.0]], calc_type="const", name="HEX_0",
-                 units=21):
+                 units=21):  # qs def.: <0=> liquid entering, qs >1 => vapor entering
 
-        calculate = True
+        
         self.temps = temps
         self.ps = ps
+        self.qs =qs
         self.points = points
         self.dT_hex = dT_hex
         self.dT_superh = dT_superh
+        self.heating =heating
         self.compositions = compositions
         self.props = props
         self.units = units
         self.calc_type = calc_type
         self.name = name
-        self.fluids =fluids
-        if calculate:
-            import sys
-            from ctREFPROP.ctREFPROP import REFPROPFunctionLibrary as modwf
+        self.fluids = fluids
 
-            from ctREFPROP.ctREFPROP import REFPROPFunctionLibrary as modsf
-            self.RP = [flp.setRPFluid(self.fluids[0], modwf, 'RPPREFIX'),
-                       flp.setRPFluid(self.fluids[1], modsf, 'RPPREFIXs')]
-            state_in = np.zeros((2, 2))
-            sat_v = flp.prop_pq(ps[0], 1.,
-                               fluids[0], compositions[0],
-                               props=props, units=units, RP=self.RP[0])
-            
-            sat_l = flp.prop_pq(ps[0], 0.,
-                               fluids[0], compositions[0],
-                               props=props, units=units, RP=self.RP[0])
-            
-            ev_out = flp.tp(sat_v[0]+dT_superh, ps[0],
-                               fluids[0], compositions[0],
-                               props=props, units=units, RP=self.RP[0])
-            
-            sf_in = flp.tp(sat_v[0]+dT_superh+ dT_hex, ps[1],
-                               fluids[1], compositions[1],
-                               props=props, units=units, RP=self.RP[1])
-            sf_out = flp.tp(sat_l[0]+ dT_hex, ps[1],
-                               fluids[1], compositions[1],
-                               props=props, units=units, RP=self.RP[1])
-            
-            print(sat_v, sat_l, ev_out, "\nsf:",sf_in, sf_out)
-            
-            dh0 = ev_out[2] - sat_l[2]
-            dh1 =sf_in[2] - sf_out[2]
-            m_ratio =dh1 / dh0
-            h_0 = np.linspace(sat_l[2], ev_out[2])
-            h_1 = np.linspace(sf_out[2], sf_in[2])
-            t0 = flp.hp_v(h_0,ps[0],
-                               fluids[0], compositions[0],
-                               props=props, units=units, RP=self.RP[0])
-            t1 = flp.hp_v(h_1,ps[1],
-                               fluids[1], compositions[1],
-                               props=props, units=units, RP=self.RP[1])
-            self.enthalpies =[h_0, h_1]
-            self.m_ratio = m_ratio
-            self.dh = [dh0,dh1]
-            self.t_all =[t0,t1]
-            self.points=[sat_l,sat_v, ev_out, sf_in, sf_out]
-                                  
-            
-            
+    def pinchpoint(self, verbose =False):
 
+     
+        from ctREFPROP.ctREFPROP import REFPROPFunctionLibrary as modwf
 
+        from ctREFPROP.ctREFPROP import REFPROPFunctionLibrary as modsf
+        self.RP = [flp.setRPFluid(self.fluids[0], modwf, 'RPPREFIX'),
+                   flp.setRPFluid(self.fluids[1], modsf, 'RPPREFIXs')]
+        sat_p_initial =self.ps[0]
+        sat_p = flp.prop_Tq(self.temps[0], 1.,
+                            self.fluids[0], self.compositions[0],
+                            props=self.props, units=self.units,
+                            RP=self.RP[0])
+        self.ps[0] = sat_p[1]
+        print(f"Saturation pressure varied from {sat_p_initial:.2e} to {self.ps[0] :.2e} Pa!")
+        sat_v = flp.prop_pq(self.ps[0], 1.,
+                            self.fluids[0], self.compositions[0],
+                            props=self.props, units=self.units,
+                            RP=self.RP[0])
+
+        sat_l = flp.prop_pq(ps[0], 0.,
+                            self.fluids[0], self.compositions[0],
+                            props=self.props, units=self.units,
+                            RP=self.RP[0])
+
+        ev_out = flp.tp(sat_v[0]+self.dT_superh, self.ps[0],
+                        self.fluids[0], self.compositions[0],
+                        props=self.props, units=self.units,
+                        RP=self.RP[0])
+
+        sf_in = flp.tp(sat_v[0]+self.dT_superh + self.dT_hex, self.ps[1],
+                       self.fluids[1], self.compositions[1],
+                       props=self.props, units=self.units,
+                       RP=self.RP[1])
+        sf_out = flp.tp(sat_l[0] + self.dT_hex, self.ps[1],
+                        self.fluids[1], self.compositions[1],
+                        props=self.props, units=self.units, RP=self.RP[1])
+
+        if verbose: print(sat_v, sat_l, ev_out, "\nsf:", sf_in, sf_out)
+
+        dh0 = ev_out[2] - sat_l[2]
+        dh1 = sf_in[2] - sf_out[2]
+        m_ratio = dh1 / dh0
+        h_0 = np.linspace(sat_l[2], ev_out[2], self.points)
+        h_1 = np.linspace(sf_out[2], sf_in[2], self.points)
+        t0 = flp.hp_v(h_0, self.ps[0],
+                      self.fluids[0], self.compositions[0],
+                      props=self.props, units=self.units, RP=self.RP[0])
+        t1 = flp.hp_v(h_1, self.ps[1],
+                      self.fluids[1], self.compositions[1],
+                      props=self.props, units=self.units, RP=self.RP[1])
+        self.enthalpies = [h_0, h_1]
+        self.m_ratio = m_ratio
+        self.dh = [dh0, dh1]
+        self.t_all = [t0, t1]
+        self.points = [sat_l, sat_v, ev_out, sf_in, sf_out]
+        h0_shifted = hp0.t_all[0][2, :] - \
+            hp0.t_all[0][2, 0], hp0.t_all[0][0, :]
+        h1_shifted = (hp0.t_all[1][2, :] - hp0.t_all[1]
+                      [2, 0])/hp0.m_ratio, hp0.t_all[1][0, :]
+        dT_min = np.min(h1_shifted[1]-h0_shifted[1])
+        if dT_min < self.dT_hex*.999:
+            
+            print(f"Problem{dT_min}")
+
+    def pp_root(self, var_in):
+        if self.heating:
+            if self.qs[0] >=0 and self.qs[0] <=1:
+                pass
+                
+        
+        
 if __name__ == "__main__":
     fl1 = "Propane*Pentane*Butane"
     fl2 = "Methanol"
-    compositions = [[.25, .3,.45], [1.]]
-    Ts = [290., 350.]
+    compositions = [[.5, .1, .4], [1.]]
+    Ts = [290., 250.]
     # fl = fl_names
     flx = [fl1, fl2]
-    ps = [1.2e5,  12e5]
-    hp0 = pinch_point(flx, Ts, ps, dT_superh=15, compositions=compositions)
+    ps = [1.92e5,  12e5]
+
+    hp0 = static_heat_exchanger(
+        flx, Ts, ps, dT_superh=15, compositions=compositions)
+    hp0.pinchpoint()
+    
     plt.figure()
-    plt.plot(hp0.t_all[0][2,:]- hp0.t_all[0][2,0], hp0.t_all[0][0,:])
-    plt.plot((hp0.t_all[1][2,:] -hp0.t_all[1][2,0])/hp0.m_ratio, hp0.t_all[1][0,:],"o")
+    plt.plot(hp0.t_all[0][2, :] - hp0.t_all[0][2, 0], hp0.t_all[0][0, :])
+    plt.plot((hp0.t_all[1][2, :] - hp0.t_all[1][2, 0]) /
+             hp0.m_ratio, hp0.t_all[1][0, :], "o")
